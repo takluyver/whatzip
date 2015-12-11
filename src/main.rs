f40bea09c9f6bf1e3169c8efabfbc786c8b2a02f
Thomas Kluyver <thomas@kluyver.me.uk>
2015-12-11 22:49:25 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use std::env;
+use std::process;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+
+enum ArchiveType {
+    Zip,
+    Gzip,
+    Unknown,
+}
+
+impl ArchiveType {
+    fn typical_extension(&self) -> Option<&str> {
+        match self {
+            &ArchiveType::Zip => Some(".zip"),
+            &ArchiveType::Gzip => Some(".gz"),
+            &ArchiveType::Unknown => None
+        }
+    }
+}
+
+
+fn check_magic_number(f: &mut File, b: &[u8]) -> bool {
+    f.seek(io::SeekFrom::Start(0)).unwrap();
+    let mut buffer = vec![0; b.len()];
+    match f.read(&mut buffer) {
+        Ok(size) => {
+            if size < b.len() {
+                false
+            } else {
+                buffer == b
+            }
+        }
+        Err(_) => {
+            false
+        }
+    }
+}
+
+fn check_zip(f: &mut File) -> bool {
+    if f.seek(io::SeekFrom::End(-22)).is_err() {
+        return false;
+    }
+    let mut end_buf = Vec::new();
+    if f.read_to_end(&mut end_buf).is_err() {
+        return false;
+    }
+
+    // TODO: If these bits fail, it may still be a zip file with an
+    // archive comment.
+    if &end_buf[.. 4] != b"PK\x05\x06" {
+        return false;
+    }
+    if &end_buf[(22 - 2)..] != b"\x00\x00" {
+        return false
+    }
+    true
+}
+
+fn detect_archive(f: &mut File) -> ArchiveType {
+    if check_zip(f) {
+        ArchiveType::Zip
+    } else if check_magic_number(f, b"\x1f\x8b") {
+        ArchiveType::Gzip
+    } else {
+        ArchiveType::Unknown
+    }
+}
+
+fn main() {
+    match env::args().nth(1) {
+        Some(s) => {
+            match File::open(&s) {
+                Ok(mut f) => {
+                    println!("OK");
+                    let at = detect_archive(&mut f);
+                    match at.typical_extension() {
+                        Some(ext) => println!("Detected {} file", ext),
+                        None => println!("Unknown file type")
+                    }
+                },
+                Err(_) => {
+                    println!("Failed to open {}", s);
+                    process::exit(1);
+                }
+            }
+        },
+        None => {
+            println!("No argument!");
+            process::exit(2);
+        },
+    }
+}
